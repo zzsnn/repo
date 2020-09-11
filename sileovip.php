f2f94f50fcf3dac1e34d20ca9795fb0fe5c72371
TheFine <768463945@qq.com>
2020-09-12 06:47:09 +0800

    更新

---
+++ sileovip.php
@@ -0,0 +1,251 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>个人中心-会员升级</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="apple-mobile-web-app-title" content="多米诺骨牌源™" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<meta name="HandheldFriendly" content="true" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="robots" content="index, follow" />
+		<meta name="title" content="多米诺骨牌源" />
+		<meta name="description" content="个人分享源，只为娱乐大众！<br />如有法律风险，本人一律不承担！" />
+		<meta name ="keywords" content="多米诺骨牌,多米诺骨牌源,微信插件,cydia插件,软件源,cydia源,越狱插件" />
+		<base target="_top">
+		<link rel="apple-touch-icon" href="//apt.wxhbts.com/CydiaIcon.png">
+		<link rel="shortcut icon" href="//apt.wxhbts.com/favicon.ico">
+		<link rel="stylesheet" href="//apt.wxhbts.com/css/menes.min.css">
+		<link rel="stylesheet" href="//apt.wxhbts.com/css/scroll.min.css">     
+		<script src="//apt.wxhbts.com/js/fastclick.js" type="text/javascript"></script>
+		<script src="//apt.wxhbts.com/js/menes.js" type="text/javascript"></script>
+		<script src="//apt.wxhbts.com/js/cytyle.js" type="text/javascript"></script>
+		<script src="//apt.wxhbts.com/js/jquery.min.js"></script>
+		 <style>
+.input-group {
+	position: relative;
+	display: table;
+	border-collapse: separate;
+	margin-left: 5px;
+    margin-right: 5px;
+}
+.input-group-addon:first-child {
+	border-right: 0;
+}
+.input-group-addon {
+	padding: 6px 12px;
+	font-size: 14px;
+	font-weight: 400;
+	line-height: 1;
+	color: #555;
+	text-align: center;
+	background-color: #eee;
+	border: 1px solid #ccc;
+	
+}
+.input-group-addon,.input-group-btn {
+	width: 1%;
+	white-space: nowrap;
+	vertical-align: middle;
+}
+.input-group .form-control,.input-group-addon,.input-group-btn {
+	display: table-cell;
+}
+.input-group .form-control {
+	position: relative;
+	z-index: 2;
+	float: left;
+	width: 100%;
+	margin-bottom: 0
+}
+.form-control {
+	display: block;
+	width: 100%;
+	height: 34px;
+	padding: 6px 12px;
+	font-size: 14px;
+	line-height: 1.42857143;
+	color: #555;
+	background-color: #fff;
+	background-image: none;
+	border: 1px solid #ccc;
+	
+
+	-webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
+	-o-transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+	transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+}
+.btn-primary {
+	color: #fff;
+	background-color: #337ab7;
+	border-color: #2e6da4
+}
+.btn {
+	
+	padding: 6px 12px;
+	margin-bottom: 0;
+	font-size: 14px;
+	font-weight: 400;
+	line-height: 1.42857143;
+	text-align: center;
+	white-space: nowrap;
+	vertical-align: middle;
+	-ms-touch-action: manipulation;
+	touch-action: manipulation;
+	cursor: pointer;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
+	background-image: none;
+	border: 1px solid transparent;
+	
+}
+</style>    
+	</head>
+	<body class="pinstripe">
+      		<panel>
+			
+     <div style="height:2px;width:100%">
+        <div style="background-color:red;height:2px;width:0" id="progress"></div>
+    </div>
+    <panel class="left">
+        <div style="margin:9px;padding:0 16px">
+            <style type="text/css">
+                .cytyle-flat #logo {
+                    background-image: url("CydiaIcon.png");
+                    background-size: 58px 58px;
+                    width: 58px
+                }
+            </style>
+            <div id="logo" style="position:absolute;display:inline-block;height:58px;vertical-align:middle" alt="Cydia Logo"></div>
+            <p style="padding-top:8px;margin-left:70px;text-align:center;display:block;height:60px;text-rendering:optimizeLegibility">
+            <span style="font-size:18px">欢迎使用 Cydia 帐户<sup>™</sup></span><br>
+			<span style="font-size:14px"><a>会员个人管理中心</a></span><br>
+            <span style="font-size:14px"><a>Build by wxhbts</a></span></p>
+        </div>
+     <block>
+        <p style="color: #303130; font-size:14px;">启用收藏插件功能,您可在不同的设备上登录同一个账号来快速安装收藏过的插件！</p>
+       <hr>
+       <p>已收藏 <strong>0</strong> 个软件包</p>
+
+    </block>
+    		<fieldset>
+              <a>
+                        <img class="icon" src="https://apt.wxhbts.com/icon/user.png" style="border-radius: 15%; box-shadow: 0 0 1px #AAA;">
+                            <div>
+                                <div>
+                                    <label>
+                                        <p>会员等级：普通会员</p> </label>
+                                </div>
+                            </div>
+                  </a>
+				  <a>
+                        <img class="icon" src="//thirdqq.qlogo.cn/g?b=oidb&k=6fAHwQBTODUYobEPmkt8dA&s=100" style="border-radius: 15%; box-shadow: 0 0 1px #AAA;">
+                            <div>
+                                <div>
+                                    <label>
+                                        <p>用户名称：他是个疯子</p><i onclick="cookipost()" class="ui-btn3" style="color: #979797; float:right;">退出</i>
+                                    </label>
+                                </div>
+                            </div>
+                  </a>
+				  <a href="https://bm.udid.xin/share_1014.html"target="_open">
+				<img class="icon" src="//apt.wxhbts.com/icon/default/Shopping.png" />
+					<div>
+						<div>
+							<label>
+								<p>购买授权：会员组价格 50 💰</p>
+							</label>
+						</div>
+					</div>
+				</a>
+			</fieldset>
+<script language="javascript">
+    function cookipost(){
+      $.get("/qqlogin/777.php", function(result){
+    	location.reload();
+  	  });  
+	}
+  </script> 
+  <fieldset class="emergency">
+					<div>
+						<div>
+							<div style="text-align:center;"><span style="color:#E53333;">如果下方无UDID 请刷新源即可<br>请关闭自带浏览器的无痕模式<br>
+请勿乱输入UDID否则造成激活失败无法下载</span></div>
+						</div>
+					</div>
+        </fieldset>
+      <form action="cydiavip.php" method="post">
+        <div class="input-group">
+            <span class="input-group-addon"style="border-bottom-left-radius: 4px;border-top-left-radius: 4px;">用户ID</span>
+            <input style="border-top-right-radius:4px;border-bottom-right-radius:4px;top: 0px;" name="id" value="3812E8D9343C52674FED143426243890"class="form-control" placeholder="请勿输入否则造成激活失败无法下载">
+        </div>   <br>
+		<div class="input-group">
+            <span class="input-group-addon"style="border-bottom-left-radius: 4px;border-top-left-radius: 4px;">设备ID</span>
+            <input style="border-top-right-radius:4px;border-bottom-right-radius:4px;top: 0px;" name="modelid" value="设备型号:iPhone XS Max系统版本:"class="form-control" placeholder="请勿输入否则造成激活失败无法下载">
+        </div>   <br>
+		<div class="input-group">
+            <span class="input-group-addon"style="border-bottom-left-radius: 4px;border-top-left-radius: 4px;">&nbsp;UDID&nbsp;</span>
+           <input type="text" name="udid" value="00008020-000974912246002E" class="form-control" minlength="40" maxlength="40" data-validation-regex-regex="[a-zA-Z0-9]*" data-validation-minlength-message="UDID必须是40个字符！" data-validation-maxlength-message="UDID必须是40个字符！" placeholder="请输入设备UDID" style="top: 0px;border-top-right-radius: 4px;border-bottom-right-radius: 4px;">
+        </div>      
+        <br/>      
+          <div class="input-group">
+            <span class="input-group-addon"style="border-bottom-left-radius: 4px;border-top-left-radius: 4px;">激活码</span>
+            <input style="border-top-right-radius:4px;border-bottom-right-radius:4px;top: 0px;" type="text" name="code" class="form-control" placeholder="输入激活码">
+        </div>   <br>
+ <div class="input-group" style="width:97.5%">
+        <button style="width:100%;border-radius:4px" type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-hdd"></span>&nbsp; 升级用户组</button>
+        </div>		
+      </form>
+<fieldset class="source" style="margin-top: 17px;">
+				<a href="/ios.php">
+					<img class="icon" src="//apt.wxhbts.com/CydiaIcon.png" />
+					<div>
+						<div>
+							<label>
+								<p id="source-name">
+									多米诺骨牌源™								</p>
+							</label>
+						</div>
+					</div>
+				</a>
+				<div class="source-description" id="source-description">
+					<p>
+						个人分享源，只为娱乐大众！
+<br />如有法律风险，本人一律不承担！					</p>
+				</div>
+			</fieldset>
+   <footer>
+				<p>
+                  <span>注册时间：2019-06-04 13:14:57</span>
+				</p>
+			</footer>
+		</panel>
+	<div style="text-align: center; display: none;">
+			<script src="https://s95.cnzz.com/z_stat.php?id=1255756703&web_id=1255756703" language="JavaScript"></script>
+		</div>	
+		<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  }
+  else{
+  bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+  }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a138a0ef7386fafec159dbf02fcc9e71";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script></body>
+ </html>
