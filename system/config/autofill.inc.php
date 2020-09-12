a8e28e487cfbe75a8e87e3999c023383a9d7a35f
TheFine <768463945@qq.com>
2020-09-12 12:04:00 +0800

    更新

---
+++ system/config/autofill.inc.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined("DCRM")) exit();
+define("AUTOFILL_EMERGENCY", "<span style=\"font-family:monospace;font-size:medium;\"><span style=\"font-family:monospace;font-size:medium;\">本源属于Netskao私人源，一切版权归站长本人所有!<br /><br /></span><span style=\"font-family:monospace;font-size:medium;\">更多资讯</span><span style=\"font-family:monospace;font-size:medium;\">本站企业福利https://zzsnn.gitee.io/RJFO/cn/<br /><br /></span><span style=\"font-family:monospace;font-size:medium;\">插件无效请不要用屏蔽越狱的插件屏蔽应用,</span><span style=\"font-family:monospace;font-size:medium;\">安装插件出现闪退情况的，请删除所有动画加速类插件即可解决</span></span><span style=\"font-family:monospace;font-size:medium;\"></span>");
+define("AUTOFILL_PRE", "com.zzsn.utools.club");
+define("AUTOFILL_NONAME", "[未命名]");
+define("AUTOFILL_MASTER", "Origamiboy");
+define("AUTOFILL_FULLNAME", "Origamiboy");
+define("AUTOFILL_EMAIL", "1620993037@qq.com");
+define("AUTOFILL_SITE", "http://zzsn.utools.club/");
+define("AUTOFILL_DESCRIPTION", "一个萌萌哒软件包。");
+define("AUTOFILL_SEO", "折纸少年(Private)");
+define("AUTOFILL_KEYWORDS", "折纸少年(Private),个人源,越狱插件源");
+define("AUTOFILL_FOOTER_NAME", "折纸少年(Private)");
+?>
---
+++ system/version.inc.php
@@ -0,0 +1,4 @@
+<?php
+/* Auto-generated version file */
+$_version = '1.7.16.7.31';
+?>
